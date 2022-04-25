ca17b7f89bf3d0e1b955434169e5303556f6e593
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-04-25 22:56:53 +0200

    Node Graph

--- AmaterasuDemo/Source/main.cpp
+++ AmaterasuDemo/Source/main.cpp
@@ -4,41 +4,161 @@
 
 #include <imgui.h>
 #include <imgui_node_editor.h>
-# define IMGUI_DEFINE_MATH_OPERATORS
-# include <imgui_internal.h>
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include <imgui_internal.h>
+
+#include <concepts>
+#include <type_traits>
+
+#include <memory>
+#include <vector>
 
 namespace ed = ax::NodeEditor;
 
-
-
 namespace AmaterasuDemo
 {
-    struct LinkInfo
-    {
-        ed::LinkId Id;
-        ed::PinId  InputId;
-        ed::PinId  OutputId;
-    };
+	// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2020/p2098r0.pdf
+	template <class T, template<class...> class Primary> struct is_specialization_of : std::false_type {};
+	template <template<class...> class Primary, class... Args> struct is_specialization_of<Primary<Args...>, Primary> : std::true_type {};
+	template <class T, template<class...> class Primary> inline constexpr bool is_specialization_of_v = is_specialization_of<T, Primary>::value;
 
-	void ImGuiEx_BeginColumn()
-	{
-		ImGui::BeginGroup();
-	}
+	template<typename... Ts> class NodeInputDefinition {};
+	template<typename... Ts> class NoteOutputDefinition {};
 
-	void ImGuiEx_NextColumn()
-	{
-		ImGui::EndGroup();
-		ImGui::SameLine();
-		ImGui::BeginGroup();
-	}
+	template <class T> concept IsNodeInputDefinition = is_specialization_of_v<T, NodeInputDefinition>;
+	template <class T> concept IsNoteOutputDefinition = is_specialization_of_v<T, NoteOutputDefinition>;
 
-	void ImGuiEx_EndColumn()
+	class INode
 	{
-		ImGui::EndGroup();
-	}
+	};
+
+	template<IsNodeInputDefinition T1, IsNoteOutputDefinition T2>
+	class Node : public INode
+	{
+	public:
+		typedef T1 InputDefinition;
+		typedef T2 OutputDefinition;
+	};
+
+	class NodeGraph2
+	{
+	public:
+		struct NodeInfo
+		{
+			INode* Data;
+			ImVec2 Position;
+			ImVec2 Size;
+			bool Active = false;
+			bool Hovered = false;
+		};
+	public:
+		NodeGraph2()
+		{
+			NodeInfo nodeInfo;
+			nodeInfo.Data = new Node<NodeInputDefinition<float, float>, NoteOutputDefinition<float>>();
+			nodeInfo.Position = ImVec2(20.0f, 20.0f);
+			nodeInfo.Size = ImVec2(250.0f, 100.0f);
+
+			m_Nodes.push_back(nodeInfo);
+		}
+
+		void Initialize() {}
+
+
+		void RenderNode(ImDrawList* drawList, ImVec2 offset, const NodeInfo& node)
+		{
+			drawList->AddRectFilled(offset + node.Position, offset + node.Position + node.Size, node.Active ? IM_COL32(90, 20, 20, 255) : IM_COL32(60, 20, 20, 255));
+		}
+
+		ImVec2 lastMousePos = ImVec2(0.0f, 0.0f);
+		ImVec2 scrolling = ImVec2(0.0f, 0.0f);
+		NodeInfo* draggingNode = nullptr;
+
+		bool mouseDown = false;
+		bool mouseClicked = false;
+		bool mouseReleased = false;
+
+		void ImGuiRender()
+		{
+			bool lastMouseDown = mouseDown;
+			bool lastMouseClicked = mouseClicked;
+			bool lastMouseReleased = mouseReleased;
+
+			mouseDown = ImGui::IsMouseDown(ImGuiMouseButton_Left);
+			mouseClicked = ImGui::IsMouseDown(ImGuiMouseButton_Left) && !lastMouseClicked;
+			mouseReleased = !ImGui::IsMouseDown(ImGuiMouseButton_Left) && lastMouseDown;
+
+			ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();
+			ImVec2 canvas_sz = ImGui::GetContentRegionAvail();
+			ImVec2 canvas_p1 = ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
+
+			// Draw border and background color
+			ImGuiIO& io = ImGui::GetIO();
+			ImDrawList* drawList = ImGui::GetWindowDrawList();
+			drawList->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(20, 20, 20, 255));
+			drawList->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 255));
+
+			// This will catch our interactions
+			ImGui::InvisibleButton("canvas", canvas_sz, ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight);
+			const bool isHovered = ImGui::IsItemHovered();
+			const bool isActive = ImGui::IsItemActive();
+			const ImVec2 origin(canvas_p0.x + scrolling.x, canvas_p0.y + scrolling.y); // Lock scrolled origin
+
+			for (NodeInfo& node : m_Nodes)
+			{
+				node.Hovered = io.MousePos.x >= origin.x + node.Position.x && io.MousePos.x <= origin.x + node.Position.x + node.Size.x && io.MousePos.y >= origin.y + node.Position.y && io.MousePos.y <= origin.y + node.Position.y + node.Size.y;
+				draggingNode = mouseClicked ? &node : (mouseReleased ? nullptr : draggingNode);
+			}
+
+			std::cout << reinterpret_cast<void*>(draggingNode) << '\n';
+
+			if (draggingNode)
+			{
+				draggingNode->Position += ImGui::GetMousePos() - lastMousePos;
+			}
+
+			drawList->PushClipRect(canvas_p0, canvas_p1, true);
+			for (const NodeInfo& node : m_Nodes)
+			{
+				RenderNode(drawList, origin, node);
+			}
+			drawList->PopClipRect();
+
+			lastMousePos = ImGui::GetMousePos();
+		}
+		void Terminate() {}
+
+	private:
+		std::vector<NodeInfo> m_Nodes;
+	};
 
 	class NodeGraph
 	{
+	private:
+		struct LinkInfo
+		{
+			ed::LinkId Id;
+			ed::PinId  InputId;
+			ed::PinId  OutputId;
+		};
+
+		void ImGuiEx_BeginColumn()
+		{
+			ImGui::BeginGroup();
+		}
+
+		void ImGuiEx_NextColumn()
+		{
+			ImGui::EndGroup();
+			ImGui::SameLine();
+			ImGui::BeginGroup();
+		}
+
+		void ImGuiEx_EndColumn()
+		{
+			ImGui::EndGroup();
+		}
+
 	public:
 		void Initialize()
 		{
@@ -46,155 +166,155 @@ namespace AmaterasuDemo
 			config.SettingsFile = "Simple.json";
 			g_Context = ed::CreateEditor(&config);
 		}
-		
+
 		void ImGuiRender()
 		{
-            auto& io = ImGui::GetIO();
+			auto& io = ImGui::GetIO();
 
-            ImGui::Text("FPS: %.2f (%.2gms)", io.Framerate, io.Framerate ? 1000.0f / io.Framerate : 0.0f);
+			ImGui::Text("FPS: %.2f (%.2gms)", io.Framerate, io.Framerate ? 1000.0f / io.Framerate : 0.0f);
 
-            ImGui::Separator();
+			ImGui::Separator();
 
-            ed::SetCurrentEditor(g_Context);
+			ed::SetCurrentEditor(g_Context);
 
-            // Start interaction with editor.
-            ed::Begin("My Editor", ImVec2(0.0, 0.0f));
+			// Start interaction with editor.
+			ed::Begin("My Editor", ImVec2(0.0, 0.0f));
 
-            int uniqueId = 1;
+			int uniqueId = 1;
 
-            //
-            // 1) Commit known data to editor
-            //
+			//
+			// 1) Commit known data to editor
+			//
 
-            // Submit Node A
-            ed::NodeId nodeA_Id = uniqueId++;
-            ed::PinId  nodeA_InputPinId = uniqueId++;
-            ed::PinId  nodeA_OutputPinId = uniqueId++;
+			// Submit Node A
+			ed::NodeId nodeA_Id = uniqueId++;
+			ed::PinId  nodeA_InputPinId = uniqueId++;
+			ed::PinId  nodeA_OutputPinId = uniqueId++;
 
-            if (g_FirstFrame)
-                ed::SetNodePosition(nodeA_Id, ImVec2(10, 10));
-            ed::BeginNode(nodeA_Id);
-            ImGui::Text("Node A");
-            ed::BeginPin(nodeA_InputPinId, ed::PinKind::Input);
-            ImGui::Text("-> In");
-            ed::EndPin();
-            ImGui::SameLine();
-            ed::BeginPin(nodeA_OutputPinId, ed::PinKind::Output);
-            ImGui::Text("Out ->");
-            ed::EndPin();
-            ed::EndNode();
+			if (g_FirstFrame)
+				ed::SetNodePosition(nodeA_Id, ImVec2(10, 10));
+			ed::BeginNode(nodeA_Id);
+			ImGui::Text("Node A");
+			ed::BeginPin(nodeA_InputPinId, ed::PinKind::Input);
+			ImGui::Text("-> In");
+			ed::EndPin();
+			ImGui::SameLine();
+			ed::BeginPin(nodeA_OutputPinId, ed::PinKind::Output);
+			ImGui::Text("Out ->");
+			ed::EndPin();
+			ed::EndNode();
 
-            // Submit Node B
-            ed::NodeId nodeB_Id = uniqueId++;
-            ed::PinId  nodeB_InputPinId1 = uniqueId++;
-            ed::PinId  nodeB_InputPinId2 = uniqueId++;
-            ed::PinId  nodeB_OutputPinId = uniqueId++;
+			// Submit Node B
+			ed::NodeId nodeB_Id = uniqueId++;
+			ed::PinId  nodeB_InputPinId1 = uniqueId++;
+			ed::PinId  nodeB_InputPinId2 = uniqueId++;
+			ed::PinId  nodeB_OutputPinId = uniqueId++;
 
-            if (g_FirstFrame)
-                ed::SetNodePosition(nodeB_Id, ImVec2(210, 60));
-            ed::BeginNode(nodeB_Id);
-            ImGui::Text("Node B");
-            ImGuiEx_BeginColumn();
-            ed::BeginPin(nodeB_InputPinId1, ed::PinKind::Input);
-            ImGui::Text("-> In1");
-            ed::EndPin();
-            ed::BeginPin(nodeB_InputPinId2, ed::PinKind::Input);
-            ImGui::Text("-> In2");
-            ed::EndPin();
-            ImGuiEx_NextColumn();
-            ed::BeginPin(nodeB_OutputPinId, ed::PinKind::Output);
-            ImGui::Text("Out ->");
-            ed::EndPin();
-            ImGuiEx_EndColumn();
-            ed::EndNode();
+			if (g_FirstFrame)
+				ed::SetNodePosition(nodeB_Id, ImVec2(210, 60));
+			ed::BeginNode(nodeB_Id);
+			ImGui::Text("Node B");
+			ImGuiEx_BeginColumn();
+			ed::BeginPin(nodeB_InputPinId1, ed::PinKind::Input);
+			ImGui::Text("-> In1");
+			ed::EndPin();
+			ed::BeginPin(nodeB_InputPinId2, ed::PinKind::Input);
+			ImGui::Text("-> In2");
+			ed::EndPin();
+			ImGuiEx_NextColumn();
+			ed::BeginPin(nodeB_OutputPinId, ed::PinKind::Output);
+			ImGui::Text("Out ->");
+			ed::EndPin();
+			ImGuiEx_EndColumn();
+			ed::EndNode();
 
-            // Submit Links
-            for (auto& linkInfo : g_Links)
-                ed::Link(linkInfo.Id, linkInfo.InputId, linkInfo.OutputId);
+			// Submit Links
+			for (auto& linkInfo : g_Links)
+				ed::Link(linkInfo.Id, linkInfo.InputId, linkInfo.OutputId);
 
-            //
-            // 2) Handle interactions
-            //
+			//
+			// 2) Handle interactions
+			//
 
-            // Handle creation action, returns true if editor want to create new object (node or link)
-            if (ed::BeginCreate())
-            {
-                ed::PinId inputPinId, outputPinId;
-                if (ed::QueryNewLink(&inputPinId, &outputPinId))
-                {
-                    // QueryNewLink returns true if editor want to create new link between pins.
-                    //
-                    // Link can be created only for two valid pins, it is up to you to
-                    // validate if connection make sense. Editor is happy to make any.
-                    //
-                    // Link always goes from input to output. User may choose to drag
-                    // link from output pin or input pin. This determine which pin ids
-                    // are valid and which are not:
-                    //   * input valid, output invalid - user started to drag new ling from input pin
-                    //   * input invalid, output valid - user started to drag new ling from output pin
-                    //   * input valid, output valid   - user dragged link over other pin, can be validated
+			// Handle creation action, returns true if editor want to create new object (node or link)
+			if (ed::BeginCreate())
+			{
+				ed::PinId inputPinId, outputPinId;
+				if (ed::QueryNewLink(&inputPinId, &outputPinId))
+				{
+					// QueryNewLink returns true if editor want to create new link between pins.
+					//
+					// Link can be created only for two valid pins, it is up to you to
+					// validate if connection make sense. Editor is happy to make any.
+					//
+					// Link always goes from input to output. User may choose to drag
+					// link from output pin or input pin. This determine which pin ids
+					// are valid and which are not:
+					//   * input valid, output invalid - user started to drag new ling from input pin
+					//   * input invalid, output valid - user started to drag new ling from output pin
+					//   * input valid, output valid   - user dragged link over other pin, can be validated
 
-                    if (inputPinId && outputPinId) // both are valid, let's accept link
-                    {
-                        // ed::AcceptNewItem() return true when user release mouse button.
-                        if (ed::AcceptNewItem())
-                        {
-                            // Since we accepted new link, lets add one to our list of links.
-                            g_Links.push_back({ ed::LinkId(g_NextLinkId++), inputPinId, outputPinId });
+					if (inputPinId && outputPinId) // both are valid, let's accept link
+					{
+						// ed::AcceptNewItem() return true when user release mouse button.
+						if (ed::AcceptNewItem())
+						{
+							// Since we accepted new link, lets add one to our list of links.
+							g_Links.push_back({ ed::LinkId(g_NextLinkId++), inputPinId, outputPinId });
 
-                            // Draw new link.
-                            ed::Link(g_Links.back().Id, g_Links.back().InputId, g_Links.back().OutputId);
-                        }
+							// Draw new link.
+							ed::Link(g_Links.back().Id, g_Links.back().InputId, g_Links.back().OutputId);
+						}
 
-                        // You may choose to reject connection between these nodes
-                        // by calling ed::RejectNewItem(). This will allow editor to give
-                        // visual feedback by changing link thickness and color.
-                    }
-                }
-            }
-            ed::EndCreate(); // Wraps up object creation action handling.
-
-
-            // Handle deletion action
-            if (ed::BeginDelete())
-            {
-                // There may be many links marked for deletion, let's loop over them.
-                ed::LinkId deletedLinkId;
-                while (ed::QueryDeletedLink(&deletedLinkId))
-                {
-                    // If you agree that link can be deleted, accept deletion.
-                    if (ed::AcceptDeletedItem())
-                    {
-                        // Then remove link from your data.
-                        for (auto& link : g_Links)
-                        {
-                            if (link.Id == deletedLinkId)
-                            {
-                                g_Links.erase(&link);
-                                break;
-                            }
-                        }
-                    }
-
-                    // You may reject link deletion by calling:
-                    // ed::RejectDeletedItem();
-                }
-            }
-            ed::EndDelete(); // Wrap up deletion action
+						// You may choose to reject connection between these nodes
+						// by calling ed::RejectNewItem(). This will allow editor to give
+						// visual feedback by changing link thickness and color.
+					}
+				}
+			}
+			ed::EndCreate(); // Wraps up object creation action handling.
 
 
+			// Handle deletion action
+			if (ed::BeginDelete())
+			{
+				// There may be many links marked for deletion, let's loop over them.
+				ed::LinkId deletedLinkId;
+				while (ed::QueryDeletedLink(&deletedLinkId))
+				{
+					// If you agree that link can be deleted, accept deletion.
+					if (ed::AcceptDeletedItem())
+					{
+						// Then remove link from your data.
+						for (auto& link : g_Links)
+						{
+							if (link.Id == deletedLinkId)
+							{
+								g_Links.erase(&link);
+								break;
+							}
+						}
+					}
 
-            // End of interaction with editor.
-            ed::End();
+					// You may reject link deletion by calling:
+					// ed::RejectDeletedItem();
+				}
+			}
+			ed::EndDelete(); // Wrap up deletion action
 
-            if (g_FirstFrame)
-                ed::NavigateToContent(0.0f);
 
-            ed::SetCurrentEditor(nullptr);
 
-            g_FirstFrame = false;
+			// End of interaction with editor.
+			ed::End();
 
-            // ImGui::ShowMetricsWindow();
+			if (g_FirstFrame)
+				ed::NavigateToContent(0.0f);
+
+			ed::SetCurrentEditor(nullptr);
+
+			g_FirstFrame = false;
+
+			// ImGui::ShowMetricsWindow();
 		}
 
 		void Terminate()
@@ -204,10 +324,10 @@ namespace AmaterasuDemo
 		}
 
 	private:
-        ed::EditorContext* g_Context = nullptr;    // Editor context, required to trace a editor state.
-        bool                 g_FirstFrame = true;    // Flag set for first frame only, some action need to be executed once.
-        ImVector<LinkInfo>   g_Links;                // List of live links. It is dynamic unless you want to create read-only view over nodes.
-        int                  g_NextLinkId = 100;     // Counter to help generate link ids. In real application this will probably based on pointer to user data structure.
+		ed::EditorContext* g_Context = nullptr;    // Editor context, required to trace a editor state.
+		bool                g_FirstFrame = true;    // Flag set for first frame only, some action need to be executed once.
+		ImVector<LinkInfo>  g_Links;                // List of live links. It is dynamic unless you want to create read-only view over nodes.
+		int                 g_NextLinkId = 100;     // Counter to help generate link ids. In real application this will probably based on pointer to user data structure.
 
 	};
 
@@ -220,7 +340,7 @@ namespace AmaterasuDemo
 		void ImGuiRender() override;
 
 	private:
-		NodeGraph m_NodeGraph;
+		NodeGraph2 m_NodeGraph;
 	};
 
 	AmaterasuDemoApplication::AmaterasuDemoApplication()
@@ -240,6 +360,8 @@ namespace AmaterasuDemo
 		Application::ImGuiRender();
 
 		m_NodeGraph.ImGuiRender();
+
+		ImGui::ShowDemoWindow();
 	}
 }
 
