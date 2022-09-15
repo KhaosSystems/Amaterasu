040ea0e4b138b92a2422a2792137aabc3a04a460
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-09-15 17:43:52 +0200

    Sanity

--- Source/Amaterasu/Core/Windows/FileDialogs.cpp
+++ Source/Amaterasu/Core/Windows/FileDialogs.cpp
@@ -9,80 +9,108 @@
 
 namespace Amaterasu
 {
-    namespace FileDialogs
-    {
-        std::string OpenFile(const char* filter)
-        {
-            OPENFILENAMEA ofn;
-            CHAR szFile[260] = { 0 };
-            CHAR currentDir[256] = { 0 };
-            ZeroMemory(&ofn, sizeof(OPENFILENAME));
-            ofn.lStructSize = sizeof(OPENFILENAME);
-            ofn.hwndOwner = NULL;
-            ofn.lpstrFile = szFile;
-            ofn.nMaxFile = sizeof(szFile);
-            if (GetCurrentDirectoryA(256, currentDir))
-                ofn.lpstrInitialDir = currentDir;
-            ofn.lpstrFilter = filter;
-            ofn.nFilterIndex = 1;
-            ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+	namespace FileDialogs
+	{
+		std::string OpenFile(const char* filter)
+		{
+			OPENFILENAMEA ofn;
+			CHAR szFile[260] = { 0 };
+			CHAR currentDir[256] = { 0 };
+			ZeroMemory(&ofn, sizeof(OPENFILENAME));
+			ofn.lStructSize = sizeof(OPENFILENAME);
+			ofn.hwndOwner = NULL;
+			ofn.lpstrFile = szFile;
+			ofn.nMaxFile = sizeof(szFile);
+			if (GetCurrentDirectoryA(256, currentDir))
+				ofn.lpstrInitialDir = currentDir;
+			ofn.lpstrFilter = filter;
+			ofn.nFilterIndex = 1;
+			ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
 
-            if (GetOpenFileNameA(&ofn) == TRUE)
-                return ofn.lpstrFile;
-            return std::string();
-        }
+			if (GetOpenFileNameA(&ofn) == TRUE)
+				return ofn.lpstrFile;
+			return std::string();
+		}
 
 
-        std::string SaveFile(const char *filter)
-        {
-            OPENFILENAMEA ofn;
-            CHAR szFile[260] = {0};
-            CHAR currentDir[256] = {0};
-            ZeroMemory(&ofn, sizeof(OPENFILENAME));
-            ofn.lStructSize = sizeof(OPENFILENAME);
-            ofn.hwndOwner = NULL;
-            ofn.lpstrFile = szFile;
-            ofn.nMaxFile = sizeof(szFile);
-            if (GetCurrentDirectoryA(256, currentDir))
-                ofn.lpstrInitialDir = currentDir;
-            ofn.lpstrFilter = filter;
-            ofn.nFilterIndex = 1;
-            ofn.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT | OFN_NOCHANGEDIR;
+		std::string SaveFile(const char* filter)
+		{
+			OPENFILENAMEA ofn;
+			CHAR szFile[260] = { 0 };
+			CHAR currentDir[256] = { 0 };
+			ZeroMemory(&ofn, sizeof(OPENFILENAME));
+			ofn.lStructSize = sizeof(OPENFILENAME);
+			ofn.hwndOwner = NULL;
+			ofn.lpstrFile = szFile;
+			ofn.nMaxFile = sizeof(szFile);
+			if (GetCurrentDirectoryA(256, currentDir))
+				ofn.lpstrInitialDir = currentDir;
+			ofn.lpstrFilter = filter;
+			ofn.nFilterIndex = 1;
+			ofn.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT | OFN_NOCHANGEDIR;
 
-            // Sets the default extension by extracting it from the filter
-            ofn.lpstrDefExt = strchr(filter, '\0') + 1;
+			// Sets the default extension by extracting it from the filter
+			ofn.lpstrDefExt = strchr(filter, '\0') + 1;
 
-            if (GetSaveFileNameA(&ofn) == TRUE)
-                return ofn.lpstrFile;
-            return std::string();
-        }
+			if (GetSaveFileNameA(&ofn) == TRUE)
+				return ofn.lpstrFile;
+			return std::string();
+		}
 
-        std::string OpenFolder(const char* filter)
-        {
-            // https://github.com/EpicGames/UnrealEngine/blob/release/Engine/Source/Developer/DesktopPlatform/Private/Windows/DesktopPlatformWindows.cpp
-            bool success = false;
+		std::string OpenFolder(const char* filter)
+		{
+			bool success = false;
+			
+			std::wstring folderName;
 
-            bool save = false;
+			std::wstring dialogTitle = L"Testing123";
 
-            IFileDialog* fileDialog;
-            if (SUCCEEDED(::CoCreateInstance(CLSID_FileOpenDialog, nullptr, CLSCTX_INPROC_SERVER, IID_IFileOpenDialog, IID_PPV_ARGS_Helper(&fileDialog))))
-            {
-                // Set up common settings
-                FileDialog->SetTitle(*DialogTitle);
-                if (!DefaultPath.IsEmpty())
-                {
-                    // SHCreateItemFromParsingName requires the given path be absolute and use \ rather than / as our normalized paths do
-                    FString DefaultWindowsPath = FPaths::ConvertRelativePathToFull(DefaultPath);
-                    DefaultWindowsPath.ReplaceInline(TEXT("/"), TEXT("\\"), ESearchCase::CaseSensitive);
+			IFileOpenDialog* fileDialog; // Unreal has a TComPtr<T> for this...  Why?
+			if (SUCCEEDED(::CoCreateInstance(CLSID_FileOpenDialog, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&fileDialog))))
+			{
+				// Set this up as a folder picker
+				{
+					DWORD dwFlags = 0;
+					fileDialog->GetOptions(&dwFlags);
+					fileDialog->SetOptions(dwFlags | FOS_PICKFOLDERS);
+				}
 
-                    TComPtr<IShellItem> DefaultPathItem;
-                    if (SUCCEEDED(::SHCreateItemFromParsingName(*DefaultWindowsPath, nullptr, IID_PPV_ARGS(&DefaultPathItem))))
-                    {
-                        FileDialog->SetFolder(DefaultPathItem);
-                    }
-                }
-            }
+				// Set up common settings
+				fileDialog->SetTitle(dialogTitle.c_str());
+				/*if (!DefaultPath.IsEmpty())
+				{
+					// SHCreateItemFromParsingName requires the given path be absolute and use \ rather than / as our normalized paths do
+					FString DefaultWindowsPath = FPaths::ConvertRelativePathToFull(DefaultPath);
+					DefaultWindowsPath.ReplaceInline(TEXT("/"), TEXT("\\"), ESearchCase::CaseSensitive);
 
-        }
-    }
+					TComPtr<IShellItem> DefaultPathItem;
+					if (SUCCEEDED(::SHCreateItemFromParsingName(*DefaultWindowsPath, nullptr, IID_PPV_ARGS(&DefaultPathItem))))
+					{
+						FileDialog->SetFolder(DefaultPathItem);
+					}
+				}*/
+
+				// Show the picker
+				if (SUCCEEDED(fileDialog->Show(NULL/*(HWND)ParentWindowHandle*/)))
+				{
+					IShellItem* result; // Unreal uses TComPtr<IShellItem> here.
+					if (SUCCEEDED(fileDialog->GetResult(&result)))
+					{
+						PWSTR pFilePath = nullptr;
+						if (SUCCEEDED(result->GetDisplayName(SIGDN_FILESYSPATH, &pFilePath)))
+						{
+							success = true;
+
+							folderName = pFilePath;
+							// TODO: Normalize directory name
+							
+							::CoTaskMemFree(pFilePath);
+						}
+					}
+				}
+			}
+
+			return "test";
+		}
+	}
 }
